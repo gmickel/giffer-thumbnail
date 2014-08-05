1d481434f6cdb8f300be15c46cfc319221b6c5c6
Gordon Mickel <gm@cyon.ch>
2014-08-05 21:50:55 +0200

    change parameters

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ function Thumbnailer() {}
 
 
 Thumbnailer.prototype.createThumbnail = function(giffer, opts, callback) {
-  var src = opts.img,
+  var img = opts.img,
     inputDir = giffer.outDir,
     outputDir = giffer.thumbDir,
     width = giffer.width,
     height = giffer.height;
 
-  var readStream = fs.createReadStream(inputDir + '/' + src);
-  im(readStream, src + '[0]')
+  var readStream = fs.createReadStream(inputDir + '/' + img);
+  im(readStream, img + '[0]')
     .resize(width, height)
     .noProfile()
     .stream(function (err, stdout, stderr) {
       if (err) {
         return callback(err);
       }
-      var writeStream = fs.createWriteStream(outputDir + '/' + dest);
+      var writeStream = fs.createWriteStream(outputDir + '/' + img);
       stdout.pipe(writeStream);
       writeStream.on('error', function(err) {
         return callback(err);
